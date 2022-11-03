64127c9e24733428aa5aca4e9f93e22f2f31aa9b
yash <chandakyu20.comp@coep.ac.in>
2022-11-03 23:39:01 +0530

    update profile for job seeker dashboard

--- my_main.py
+++ my_main.py
@@ -29,7 +29,7 @@ def login():
             present=command_handler.fetchone()
             if present:
                 session['admin'] = 'admin'
-                return redirect(url_for('job_seeker_dashboard'))
+                return redirect(url_for('job_seeker_dashboard', name=present[0], jsid=present[1]))
     
     return render_template('login.html')
 
@@ -37,6 +37,7 @@ def login():
 def employer_dashboard(name,eid):
     # print(name)
     fulljob=[]
+    profile=[]
     if 'admin' not in session:
         return redirect(url_for('login'))
     command_handler.execute("SELECT id, role, available FROM job WHERE eid=%s", (eid,))
@@ -61,25 +62,101 @@ def employer_dashboard(name,eid):
             command_handler.execute("SELECT * FROM job WHERE id = %s",(jobid,))
             fulljob = command_handler.fetchone()
         
-        if request.method == 'POST' and 'sub3' in request.form:
-            fetch = ['ntiming', 'nrequirement', 'ncity', 'nrole', 'nsalary', 'nwork_location']
+    if request.method == 'POST' and 'sub3' in request.form:
+        fetch = ['ntiming', 'nrequirement', 'ncity', 'nrole', 'nsalary', 'nwork_location']
 
-            query_vals = []
+        query_vals = []
+    
+        for i in range(len(fetch)):
+            query_vals.append(request.form[fetch[i]])
+        query_vals.append(eid)
+
+        command_handler.execute("INSERT INTO job (available, timing, requirement, city, role, salary, work_location, eid) VALUES (1, %s, %s, %s, %s, %s,  %s, %s)", query_vals)
+
+        db.commit()
+
+    command_handler.execute("SELECT * FROM employer where id=%s", (eid,))
+    profile=command_handler.fetchone()
+    # print(profile)
+
+    if request.method == 'POST' and 'sub4' in request.form:
         
-            for i in range(len(fetch)):
-                query_vals.append(request.form[fetch[i]])
-            query_vals.append(eid)
+        fetch = ['name', 'age', 'gender', 'education', 'mobile', 'area', 'city', 'email_id', 'password']
 
-            command_handler.execute("INSERT INTO job (available, timing, requirement, city, role, salary, work_location, eid) VALUES (1, %s, %s, %s, %s, %s,  %s, %s)", query_vals)
+        query_vals = []
+    
+        for i in range(len(fetch)):
+            query_vals.append(request.form[fetch[i]])
+        # query_vals.append(eid)
 
-            db.commit()
+        command_handler.execute("UPDATE employer SET name=%s, age=%s, gender=%s, education=%s, mobile=%s, area=%s, city=%s, email_id=%s, password=%s", query_vals)
 
-    return render_template('employer_dashboard.html', name=name, jobs=lis, fulljob=fulljob)
+        db.commit()
+
+    return render_template('employer_dashboard.html', name=name, jobs=lis, fulljob=fulljob, profile=profile)
 
 
-@app.route('/job_seeker_dashboard', methods=['GET', 'POST'])
-def job_seeker_dashboard():
-    pass
+@app.route('/job_seeker_dashboard<name><jsid>', methods=['GET', 'POST'])
+def job_seeker_dashboard(name, jsid):
+    # print(name)
+    # fulljob=[]
+    profile=[]
+    if 'admin' not in session:
+        return redirect(url_for('login'))
+    # command_handler.execute("SELECT id, role, available FROM job WHERE eid=%s", (eid,))
+    # lis=command_handler.fetchall()
+    # # print(lis)
+    # if request.method=='POST' and 'jobid' in request.form:
+    #     jobid=request.form['jobid']
+    #     command_handler.execute("SELECT * FROM job WHERE id=%s", (jobid,))
+    #     fulljob = command_handler.fetchone()
+    #     # print(fulljob)
+
+    #     if request.method == 'POST' and 'sub2' in request.form:
+    #         fetch = ['available','timing','requirement','city','role','salary','work_location']
+    #         query_vals = []
+            
+    #         for i in range(0, len(fetch)):
+    #             query_vals.append(request.form[fetch[i]])
+        
+    #         command_handler.execute("UPDATE job SET available=%s, timing=%s, requirement=%s,city=%s,role=%s,salary=%s,work_location=%s", query_vals)
+    #         db.commit()
+            
+    #         command_handler.execute("SELECT * FROM job WHERE id = %s",(jobid,))
+    #         fulljob = command_handler.fetchone()
+        
+    # if request.method == 'POST' and 'sub3' in request.form:
+    #     fetch = ['ntiming', 'nrequirement', 'ncity', 'nrole', 'nsalary', 'nwork_location']
+
+    #     query_vals = []
+    
+    #     for i in range(len(fetch)):
+    #         query_vals.append(request.form[fetch[i]])
+    #     query_vals.append(eid)
+
+    #     command_handler.execute("INSERT INTO job (available, timing, requirement, city, role, salary, work_location, eid) VALUES (1, %s, %s, %s, %s, %s,  %s, %s)", query_vals)
+
+    #     db.commit()
+
+    command_handler.execute("SELECT * FROM job_seeker where id=%s", (jsid,))
+    profile=command_handler.fetchone()
+    # print(profile)
+
+    if request.method == 'POST' and 'sub4' in request.form:
+        
+        fetch = ['name', 'age', 'gender', 'education', 'mobile', 'area', 'city', 'email_id', 'password']
+
+        query_vals = []
+    
+        for i in range(len(fetch)):
+            query_vals.append(request.form[fetch[i]])
+        # query_vals.append(eid)
+
+        command_handler.execute("UPDATE job_seeker SET name=%s, age=%s, gender=%s, education=%s, mobile=%s, area=%s, city=%s, email_id=%s, password=%s", query_vals)
+
+        db.commit()
+
+    return render_template('job_seeker_dashboard.html', name=name, profile=profile)
 
 @app.route('/employer_registration', methods=['GET', 'POST'])
 def employer_registration():
